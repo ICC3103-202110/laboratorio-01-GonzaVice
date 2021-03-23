9840db4f1a3f2ec3034397fdc5418d8f7a9ea1c5
Gonzalo Vicente Tenorio <givicente@miuandes.cl>
2021-03-23 13:57:36 -0300

    Generación de lista terminada

--- Lab1ListDebug.py
+++ Lab1ListDebug.py
@@ -1,4 +1,4 @@
-## Código: Memorice para 2 Jugadores
+## Código: Memorice para 2 Jugadores (Debug de la lista)
 ## Autor: Gonzalo Ignacio Vicente Tenorio
 ## Fecha: 23-03-2021
 ## Versión: 1.2
@@ -17,21 +17,33 @@ import random as rd
 # Salida: lista
 def genList(lst, numCards):
 
-    # Mientras que el largo de la lista sea menor que la cantidad de pares de cartas solicitadas
+    print("numCards = " + str(numCards)) #SHADOWEYE
+    print("lst = " + str(lst)) #SHADOWEYE
+
+    # Mientras que el largo de la lista sea menor que la cantidad de 
+    # pares de cartas solicitadas
     while(len(lst) < numCards * 2):
 
-        # Se crea una variable llamada doble, que detectará si hay dos numeros iguales en la lista
+        print("len(lst) = " + str(len(lst)) + " aun es menor que numCards * 2 = " + str(numCards * 2)) #SHADOWEYE
+
+        # Se crea una variable llamada doble, que detectará si
+        # hay dos numeros iguales en la lista
         doble = 0
 
         # Escoje un numero al azar entre uno hasta el numero seleccionado
         randomNum = rd.randint(1, numCards)
 
+        print("randomNum = " + str(randomNum)) #SHADOWEYE
+
         # Se crea una variable para iterar en la lista de las cartas
         iteracion = 0
 
-        # Se crea la longitud original de la lista, ya que más adelante, al agregar un elemento a la lista,
-        # la longitud cambia, por lo que al iterar la lista queremos conservar la longitud inicial
+        # Se crea la longitud original de la lista, ya que más adelante,
+        # al agregar un elemento a la lista, la longitud cambia, por lo que 
+        # al iterar la lista queremos conservar la longitud inicial
         originalLenght = len(lst)
+
+        print("originalLenght = " + str(originalLenght)) #SHADOWEYE
 
         # Si la lista está vacía
         if(originalLenght == 0):
@@ -39,16 +51,23 @@ def genList(lst, numCards):
                 # Se agrega el primer número
                 lst.append(randomNum)
 
+                print("Felicidades, el primer numero agregado " + str(randomNum) + " a la lista lst = " + str(lst)) #SHADOWEYE
+
         # Si la lista no está vacía
         else:
-            # Mientras que la posición de iteracion sea menor que la longitud inicial de la lista
+            # Mientras que la psoción de iteracion sea menor que la longitud inicial de la lista
             while(iteracion < originalLenght):
 
-                # Si hay una similitud entre el numero random y el numero en la posición iterada de la lista
+                print("iteracion = " + str(iteracion)) #SHADOWEYE
+
+                # Si hay una similitud entre el numero random y el numero en la
+                # posición iterada de la lista
                 if(lst[iteracion] == randomNum):
 
                     # Aumentamos la cantidad de dobles o copias
                     doble += 1
+
+                    print("doble = " + str(doble)) #SHADOWEYE
                 
                 # Se itera al siguiente elemento de la lista
                 iteracion += 1
@@ -58,8 +77,11 @@ def genList(lst, numCards):
 
                 # Se agrega el número a la lista
                 lst.append(randomNum)
+                print("Felicidades, se agrego " + str(randomNum) + " a la lista lst = " + str(lst)) #SHADOWEYE
 
-        # El proceso se repetirá hasta que la longitud de la lista sea igual a la solicitada
+        # El proceso se repetirá hasta que la longitud de
+        # la lista sea igual a la solicitada
+        print("\n¡¡¡VUELTA!!!\n")  #SHADOWEYE
     
     #Retorna la lista generada
     return lst
@@ -72,6 +94,7 @@ print("\n\n   \'¡MEMORICE!\'   \n\n")
 
 # Cantidad de pares de tarjetas
 numPares = int(input("Escoje la cantidad de pares de cartas: "))
+# print("numPares = " + str(numPares)) # SHADOWEYE
 
 # Lista de Cartas
 cards = []
